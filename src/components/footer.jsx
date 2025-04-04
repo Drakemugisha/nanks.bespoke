1c33952b05fed48f61aad96ea60dd27a8958b5d5
mugisha drake <drakemugisha6@gmail.com>
2025-04-04 09:57:55 +0300

    phone

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../styles/footer.css';
-import { FaMailBulk, FaTwitter, FaInstagram } from 'react-icons/fa';
+import {FaPhone ,FaMailBulk, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 
 export default function Footer() {
@@ -10,6 +10,7 @@ export default function Footer() {
             <footer className="flex flex-col">
                 <div className="link flex">
                     <div className="social flex gap-4 m-6 p-3 text-xl">
+                        <a href="tel:+256789132421" className="flex gap-1 items-center"><FaPhone/> +256 789 132 421 </a>
                         <a href="https://www.instagram.com/nanksbespoke?igsh=djNnY21tdDRhYXEx&utm_source=qr" className="flex gap-1 items-center"><FaInstagram/>@nanksbespoke </a>
                         <a href="https://x.com/nanksbespoke?s=11" className="flex gap-1 items-center"><FaTwitter/> @nankbespoke </a>
                         <a href="mailto:nanksbespoke@gmail.com" className="flex gap-1 items-center"><FaMailBulk/> nanksbespoke@gmail.com </a>
